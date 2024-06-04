d079b82e1b4cdcd72b8739fa0cd20334eeabd8e4
spaceix <spacepay1486@gmail.com>
2024-06-04 18:13:44 +0900

    audio-1

--- Assets/Scripts/Options/AudioManager.cs
+++ Assets/Scripts/Options/AudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,4 +8,14 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSound;
+
+    public void PlayMusic(string name)
+    {
+        Sound sound = Array.Find(musicSounds, x=> x.name == name);
+
+        if(sound == null) 
+        {
+            Debug.Log("Sound Not Found");
+        }
+    }
 }
--- Assets/Scripts/Options/Sound.cs
+++ Assets/Scripts/Options/Sound.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [System.Serializable]
 public class Sound : MonoBehaviour
 {
-    public new string name;
+    public string name;
     public AudioClip clip;
+
+
 }
