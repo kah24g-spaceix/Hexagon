7287c13713201d8538011849e97e028b82a45fa1
spaceix <spacepay1486@gmail.com>
2024-08-29 16:03:21 +0900

    ESC

--- Assets/Scripts/UIView.cs
+++ Assets/Scripts/UIView.cs
@@ -6,19 +6,52 @@ public class UIView : MonoBehaviour
 {
     [Header("Option UI")]
     [SerializeField] private GameObject OptionUI;
-
+    private bool option;
     [Header("InGame UI")]
     [SerializeField] private GameObject TechTreeUI;
     // Start is called before the first frame update
     private void Start()
     {
-        OptionUI.gameObject.SetActive(false);
-        TechTreeUI.gameObject.SetActive(false);
+        HideUI(OptionUI);
+        HideUI(TechTreeUI);
+        option = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            option = !option;
+            CurrentActive(OptionUI, option);
+            Pause(option);
+        }
+    }
 
+
+
+
+    private void HideUI(GameObject gameObject)
+    {
+        gameObject.gameObject.SetActive(false);
+    }
+    private void ShowUI(GameObject gameObject)
+    {
+        gameObject.gameObject.SetActive(true);
+    }
+    private void CurrentActive(GameObject gameObject, bool active)
+    {
+        gameObject.gameObject.SetActive(active);
+    }
+    private void Pause(bool active)
+    {
+        if (active)
+        {
+            Time.timeScale = 0; // 게임 정지
+        }
+        else
+        {
+            Time.timeScale = 1; // 게임 재개
+        }
     }
 }
